4142724c905c532d64ada5ded7521924bdff95d8
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-26 14:35:13 +0630

    show a spinner while loading

--- src/Components/GenereList.tsx
+++ src/Components/GenereList.tsx
@@ -1,16 +1,18 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenere from "../hooks/useGenere";
 import getCroppedImgUrl from "../services/image-url";
 
 const GenereList = () => {
-  let { data } = useGenere();
+  let { data , isLoading, error} = useGenere();
+
+  if (error) return null;
+  if (isLoading) return <Spinner></Spinner>
 
   return (
     <List>
       {data.map((m) => (
         <ListItem key={m.id} paddingY={'5px'}>
           <HStack>
-
           <Image src={getCroppedImgUrl(m.image_background)} boxSize={'34px'} borderRadius={'4px'}></Image>
           <Text fontSize={'lg'}>{m.name}</Text>
           </HStack>

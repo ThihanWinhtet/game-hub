cd9bc0c48543aa2724c381e79e77e88cfcf8393d
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-28 00:54:38 +0630

    build search input

---
+++ src/Components/SearchInput.tsx
@@ -0,0 +1,13 @@
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
+import { BsSearch } from "react-icons/bs"
+
+const SearchInput = () => {
+  return (
+    <InputGroup>
+        <InputLeftElement children={<BsSearch/>}></InputLeftElement>
+      <Input placeholder="Search games..." borderRadius={20} variant='filled'></Input>
+    </InputGroup>
+  )
+}
+
+export default SearchInput
